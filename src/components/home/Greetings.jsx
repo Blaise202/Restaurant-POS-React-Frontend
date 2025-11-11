a9492044285a41c492a1ce93cca8d8b47c67e985
Blaise202 <iblaise2023@gmail.com>
2025-11-11 14:35:15 +0000

    Components/Greetings

---
+++ src/components/home/Greetings.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+export const Greetings = () => {
+  const [dateTime, setDateTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => setDateTime(new Date()), 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  const formatDate = (date) => {
+    const months = [
+      "January",
+      "February",
+      "Match",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    return `${months[date.getMonth()]}, ${String(date.getDate()).padStart(
+      2,
+      "0"
+    )} ${date.getFullYear()}`;
+  };
+
+  const formatTime = (date) => {
+    return `${String(date.getHours()).padStart(2, "0")}:${String(
+      date.getMinutes()
+    ).padStart(2, "0")}:${String(date.getSeconds()).padStart(2, "0")}`;
+  };
+
+  return (
+    <div className="flex justify-between items-center px-8 mt-5">
+      <div>
+        <h1 className="text-[#f5f5f5] text-2xl mb-2 text-semibold tracking-wide">
+          Good Morning, Blaise
+        </h1>
+        <p className="text-[#ababab] text-sm">
+          Give your best service to the customers 😁
+        </p>
+      </div>
+      <div>
+        <h1 className=" text-[#f5f5f5] text-3xl font-bold tracking-wide w-[130px]">
+          {formatTime(dateTime)}
+        </h1>
+        <p className="text-[#ababab] text-sm">{formatDate(dateTime)}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Greetings;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Bottom from "../components/include/Bottom";
+import Greetings from "../components/home/Greetings";
 
 export function Home() {
   return (
     <>
       <section className="bg-[#1f1f1f] h-[calc(100vh-4.5rem)] overflow-hidden flex gap-3">
         {/* left div */}
-        <div className="flex-[3] bg-[#589693]"></div>
+        <div className="flex-[3]">
+          <Greetings />
+        </div>
         {/* right div */}
         <div className="flex-[2] bg-[#499586]"></div>
         <Bottom />
