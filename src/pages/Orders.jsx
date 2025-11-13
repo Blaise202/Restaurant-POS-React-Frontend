df1f560825f40290b638f93ed063d562992a3c8e
Blaise202 <iblaise2023@gmail.com>
2025-11-13 15:20:11 +0000

    Orders subnav

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -9,11 +9,28 @@ import PopularDishes from "../components/home/PopularDishes";
 
 export function Orders() {
   return (
-    <>
-      <section className="bg-[#1f1f1f] h-[calc(100vh-4.5rem)] overflow-auto flex gap-3 no-scrollbar">
-        <Bottom />
-      </section>
-    </>
+    <section className="bg-[#1f1f1f] h-[calc(100vh-4.5rem)] no-scrollbar">
+      <div className="flex items-center justify-between px-10 py-4">
+        <h1 className="text-[#f5f5f5] text-lg font-semibold tracking-wide">
+          Orders
+        </h1>
+        <div className="flex items-center justify-around gap-4">
+          <button className="text-[#ababab] text-md bg-[#383838] rounded-lg px-5 py-2 font-semibold">
+            All
+          </button>
+          <button className="text-[#ababab] text-md rounded-lg px-5 py-2 font-semibold">
+            In Pogress
+          </button>
+          <button className="text-[#ababab] text-md rounded-lg px-5 py-2 font-semibold">
+            Ready
+          </button>
+          <button className="text-[#ababab] text-md rounded-lg px-5 py-2 font-semibold">
+            Completed
+          </button>
+        </div>
+      </div>
+      <Bottom />
+    </section>
   );
 }
 
--- src/pages/Tables.jsx
+++ src/pages/Tables.jsx
@@ -3,7 +3,16 @@ import Bottom from "../components/include/Bottom";
 
 export const Tables = () => {
   return (
-    <section className="bg-[#1f1f1f] h-[calc(100vh-4.5rem)] overflow-auto flex gap-3 no-scrollbar">
+    <section className="bg-[#1f1f1f] h-[calc(100vh-4.5rem)] overflow-auto no-scrollbar">
+      <div className="flex items-center justify-between px-8 py-4">
+        <h2 className="">Orders</h2>
+        <div className="">
+          <button className="">All</button>
+          <button className="">In Pogress</button>
+          <button className="">Ready</button>
+          <button className="">Completed</button>
+        </div>
+      </div>
       <Bottom />
     </section>
   );
