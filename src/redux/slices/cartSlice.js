d6c42427b05ef77cd9358a3d82b274ba31dd4783
Blaise202 <iblaise2023@gmail.com>
2025-11-28 17:34:22 +0000

    cart slice

---
+++ src/redux/slices/cartSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {}
+
+const cartSlice = createSlice({
+  name: "Cart",
+  initialState,
+  reducers: {
+    addItem: (state, action) => {
+      state.push(action.payload)
+    },
+
+    removeItem: (state, action) => {
+      return state.filter(item => item.id != action.payload)
+    }
+  }
+})
+
+export const { addItem, removeItem } = createSlice.actions
+export default cartSlice.reducer
