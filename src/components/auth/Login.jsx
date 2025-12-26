35bd318e15838e236499289c667d4777617aade6
Blaise202 <iblaise2023@gmail.com>
2025-12-26 18:34:58 +0200

    login handle on change

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <form
+      onSubmit={handleSubmit}
       action="#"
       method="post"
       className="p-3 w-[90%] my-2 rounded-lg justify-center text-xs"
@@ -16,6 +31,9 @@ export const Login = () => {
         </label>
         <input
           type="email"
+          required
+          value={formData.email}
+          onChange={handleChange}
           className="w-full block mt-2 bg-[#161616] p-3 rounded-lg"
           id="email"
           name="email"
@@ -31,13 +49,19 @@ export const Login = () => {
         </label>
         <input
           type="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
           className="w-full block mt-2 bg-[#161616] p-3 rounded-lg"
           id="password"
           placeholder="••••••••••••"
           name="password"
         />
       </div>
-      <button className="w-full font-bold bg-yellow-500 p-2 rounded-lg mt-4 text-black">
+      <button
+        type="submit"
+        className="w-full font-bold bg-yellow-500 p-2 rounded-lg mt-4 text-black"
+      >
         Login
       </button>
     </form>
